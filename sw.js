ed3ae82ca7432b7eb76d59ae964fa8c9cf0a96ab
yutoloto <loto04loto19@gmail.com>
2026-06-10 03:15:09 +0900

    Revert to working JSX version with CDN + SW offline caching

--- sw.js
+++ sw.js
@@ -1,5 +1,29 @@
-﻿const CACHE = "nakayama-v1";
-const FILES = ["/mahjong-score/", "/mahjong-score/index.html", "/mahjong-score/icon-192.png", "/mahjong-score/icon-512.png"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener("activate", e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k!==CACHE).map(k => caches.delete(k))))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+﻿const CACHE = "nakayama-v2";
+const URLS = [
+  "/mahjong-score/",
+  "/mahjong-score/index.html",
+  "/mahjong-score/manifest.json",
+  "/mahjong-score/icon-192.png",
+  "/mahjong-score/icon-512.png",
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "https://cdn.tailwindcss.com"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
