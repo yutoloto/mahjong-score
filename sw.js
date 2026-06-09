15e34556f5f0cb03cb90101e0c821c50ab63d2e5
yutoloto <loto04loto19@gmail.com>
2026-06-10 00:57:07 +0900

    Initial commit: 三麻スコア PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+const V = 'v5';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
