5fea097d7037c9e68ddd982d2468aedc813dc026
yutoloto <loto04loto19@gmail.com>
2026-06-10 02:42:18 +0900

    Add PWA support: manifest, service worker, icons, offline-first

--- sw.js
+++ sw.js
@@ -1,18 +1,5 @@
-const V = 'v6';
-const FILES = ['/', '/index.html', '/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(V).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
-});
+﻿const CACHE = "nakayama-v1";
+const FILES = ["/mahjong-score/", "/mahjong-score/index.html", "/mahjong-score/icon-192.png", "/mahjong-score/icon-512.png"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k!==CACHE).map(k => caches.delete(k))))));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
